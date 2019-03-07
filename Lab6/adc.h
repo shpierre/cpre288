2baa7d3a9b214bb60b524d6396aed297dc0c7dd9
aidans <aidans@iastate.edu>
2019-03-06 18:21:22 -0600

    completed lab 6

--- Lab6/adc.h
+++ Lab6/adc.h
@@ -5,9 +5,16 @@
  *      Author: Aidan
  */
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "timer.h"
+#include "lcd.h"
+#include <inc/tm4c123gh6pm.h>
 #ifndef ADC_H_
 #define ADC_H_
-
-
+// Initialize ADC
+void adc_init(void);
+// read distance with ADC
+int adc_read(void);
 
 #endif /* ADC_H_ */
--- Lab6/main.c
+++ Lab6/main.c
@@ -10,7 +10,8 @@
 #include "lcd.h"
 #include <inc/tm4c123gh6pm.h>
 #include "driverlib/interrupt.h"
-#include "button.h"
+#include "adc.h"
+#include <math.h>
 #include "uart.h"
 
 extern volatile char buffer[21]; // create our line buffer
@@ -20,7 +21,34 @@ extern volatile int len; // number of chars printed this line
 /**
  * main.c
  */
+//convert
+int getDist(int i){
+    int dist;
+    // calibrate our distance to our line of best fit from our test data
+    dist = 240321 * pow(i, -1.284) + 1;
+    return dist;
+}
+
 int main(void)
 {
+    //inits
+    lcd_init();
+    adc_init();
+    uart_init();
+    timer_waitMillis(500);
+    //send data headers
+    uart_sendStr("reading,distance\n\r");
+    while(true){
+
+        //Get result from adc_read
+        int fifo_result = adc_read();
+        //Convert analog value to distance (cm)
+        int distance = getDist(fifo_result);
+        char str1[80];
+        sprintf(str1, "%d,%d\n\r", fifo_result, distance);
+        lcd_printf(str1); //display info on lcd screen
+        uart_sendStr(str1); // sent string to putty using uart
+        timer_waitMillis(500); // wait 500 milliseconds
+    }
 
 }
